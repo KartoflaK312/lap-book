4b0195151c6f728038b2d3d496d61b6085e0bffd
Dominik <dominikhrynkiewicz1@gmail.com>
2023-11-24 21:36:03 +0100

    lapbook historia

---
+++ app/lapbook.tsx
@@ -0,0 +1,12 @@
+import styles from './page.module.css'
+
+export default function Lapbook({} : {}){
+
+    return (
+        <div className={styles.lapBook}>
+          <div>seks</div>
+          <div>seks2</div>
+          <div>seks3</div>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Lapbook from './lapbook'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      
+      <header className={styles.header}><h1>Lap Book - Kinematograf</h1></header>
+      <div className={styles.center}><div className={styles.lapBookContainer}>
+        <Lapbook/>
+      </div></div>
     </main>
   )
 }
